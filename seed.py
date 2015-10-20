492e582c76c7754aa6a6fb3d6796e895f216515d
Hackbright Student <info@hackbrightacademy.com>
2015-10-19 18:02:48 -0700

    Initial commit

---
+++ seed.py
@@ -0,0 +1,101 @@
+"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+
+
+from model import User
+# from model import Rating
+from model import Rating
+# from model import Movie
+from model import Movie
+
+from datetime import datetime
+from model import connect_to_db, db
+from server import app
+
+
+def load_users():
+    """Load users from u.user into database."""
+
+    print "Users"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    User.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.user"):
+        row = row.rstrip()
+        user_id, age, gender, occupation, zipcode = row.split("|")
+
+        user = User(user_id=user_id,
+                    age=age,
+                    zipcode=zipcode)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def load_movies():
+    """Load movies from u.item into database."""
+
+    print "Movies"
+
+    Movie.query.delete()
+
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        split_row = row.split("|")
+
+        movie_id = split_row[0]
+        title = split_row[1]
+        released_at = split_row[2]
+        imdb_url = split_row[4]
+
+        title.rstrip("()1234567890")
+
+        released_at = datetime.strptime(released_at, "%d-%b-%Y")
+
+        if title != "unknown":
+            movie = Movie(movie_id=movie_id,
+                            title=title,
+                            released_at=released_at,
+                            imdb_url=imdb_url)
+
+            db.session.add(movie)
+
+    db.session.commit()
+
+
+
+def load_ratings():
+    """Load ratings from u.data into database."""
+
+    print "Ratings"
+
+    Rating.query.delete()
+
+    for row in open('seed_data/u.data'):
+        row = row.rstrip()
+
+        # TODO: Split on whitespace; no pipes are present in data file. (Take another look.)
+        user_id, movie_id, score, timestamp = row.split("|")
+
+        rating = Rating(user_id=user_id, movie_id=movie_id, score=score, 
+                        timestamp=timestamp)
+
+        db.session.add(rating)
+
+    db.session.commit()
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_users()
+    load_movies()
+    load_ratings()
